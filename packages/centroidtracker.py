a5cd196e1a0bdd258ace1895a48e12b1254f17ac
Gribesh <gribeshdhakal@gmail.com>
2020-08-23 00:45:40 +0545

    created CentroidTracker class

--- packages/centroidtracker.py
+++ packages/centroidtracker.py
@@ -1,3 +1,12 @@
 from scipy.spatial import distance as dist
 from collections import OrderedDict
 import numpy as np
+
+
+class CentroidTracker(object):
+    def __init__(self, maxDisappeared=50, maxDistance=50):
+        self.nextObjectID = 0
+        self.objects = OrderedDict()
+        self.disappeared = OrderedDict()
+        self.maxDisappeared = maxDisappeared
+        self.maxDistance = maxDistance
