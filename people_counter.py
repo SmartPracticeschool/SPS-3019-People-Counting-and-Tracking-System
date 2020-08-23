04369fc920b841ff2a2b6408c7cf825639416e00
Gribesh <gribeshdhakal@gmail.com>
2020-08-23 19:04:01 +0545

    imported required libraries

---
+++ people_counter.py
@@ -0,0 +1,10 @@
+from packages.centroidtracker import CentroidTracker
+from packages.trackableobject import TrackableObject
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import time
+import dlib
+import cv2
