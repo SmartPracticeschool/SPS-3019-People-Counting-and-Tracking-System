7a65a09f9cf0ba80569eb524aa98c97f2962aae9
Gribesh <gribeshdhakal@gmail.com>
2020-08-23 19:12:54 +0545

    loaded neccessary classes and code to take either video or webcam

--- people_counter.py
+++ people_counter.py
@@ -8,3 +8,33 @@ import imutils
 import time
 import dlib
 import cv2
+
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+           "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+           "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+           "sofa", "train", "tvmonitor"]
+
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(
+    r"modelfiles/MobileNetSSD_deploy.prototxt", r"modelfiles/MobileNetSSD_deploy.caffemodel")
+
+if not (r"videos/example_01.mp4", False):
+    print("[INFO] starting video stream...")
+    vs = VideoStream(src=0).start()
+    time.sleep(2.0)
+
+else:
+    print("[INFO] opening video file...")
+    vs = cv2.VideoCapture("videos/example_01.mp4")
+
+writer = None
+W = None
+H = None
+ct = CentroidTracker(maxDisappeared=40, maxDistance=50)
+trackers = []
+trackableObjects = {}
+totalFrames = 0
+totalDown = 0
+totalUp = 0
+skip_frames = 30
+fps = FPS().start()
